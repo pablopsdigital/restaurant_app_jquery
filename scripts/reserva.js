d56f389098b70ba55e67d94309b4f651dad3a818
pablopsdigital <pablopsdigital@gmail.com>
2020-05-24 23:50:56 +0200

    version01

--- scripts/reserva.js
+++ scripts/reserva.js
@@ -2,7 +2,7 @@
 var url = "../controllers/reservaController.php";
 
 //====================================================================================================================================
-//
+//Función general consultar reservas
 //====================================================================================================================================
 function consultarReservas() {
   $.ajax({
@@ -43,9 +43,10 @@ function consultarReservas() {
 }
 
 //====================================================================================================================================
-//
+//Función consultar una reserva concreta
 //====================================================================================================================================
 function consultarReserva(idReserva) {
+  limpiarMensajesErrorFormulario();
   $.ajax({
     url: url,
     type: "POST",
@@ -60,11 +61,32 @@ function consultarReserva(idReserva) {
       document.getElementById("nombre").value = res.nombre;
       document.getElementById("apellidos").value = res.apellidos;
       document.getElementById("telefono").value = res.telefono;
-      document.getElementById("fecha").value = res.fecha;
-      document.getElementById("hora").value = res.hora;
-      $("#comensalesOptions").val(res.comensales);
-      //document.getElementById('comensales').value = res.comensales;
+
+      //Crear cadena fecha
+      var fechaBD = new Date(res.fecha);
+      var dia = fechaBD.getDate();
+      var mes = fechaBD.getMonth() + 1;
+      var anno = fechaBD.getFullYear();
+      var horas = fechaBD.getHours();
+      var minutos = fechaBD.getMinutes();
+
+      //Formatear fechas
+      if (mes < 10) mes = "0" + mes;
+      if (dia < 10) dia = "0" + dia;
+      if (horas < 10) horas ="0" + horas;
+      if (minutos <10) minutos="0" + minutos;
+      var fechaFinal = anno + "-" + mes + "-" + dia;    
+      var horaFinal = horas + ':' + minutos;
+      
+      $("#fecha").attr("value", fechaFinal);
+      $("#hora").attr("value", horaFinal);
+      document.getElementById("comensales").value = res.comensales;
       document.getElementById("comentarios").value = res.comentarios;
+
+      //Buscar boton e sustituir comportamiento
+      var boton = $('#botonGuardarModalReserva').val('Actualizar');
+      boton.removeAttr('type');
+      boton.click(modificarReserva);
     })
     .fail(function (res) {
       console.log("ERROR: en metodo consultarReserva: " + res.telefono);
@@ -72,7 +94,7 @@ function consultarReserva(idReserva) {
 }
 
 //====================================================================================================================================
-//
+//Función consultar el número de reservas para las próximas 24 horas
 //====================================================================================================================================
 function consultarReservas24horas() {
   $.ajax({
@@ -104,17 +126,17 @@ function consultarReservas24horas() {
         html += "</tr>";
       });
 
-      document.getElementById("datosReservas").innerHTML = html;
+      document.getElementById("datosReservas24horas").innerHTML = html;
       totalReservas24horas();
     })
     .fail(function (res) {
-      console.log("ERROR: en metodo consultarReservas24horas: " + res.telefono);
+      console.log("ERROR: en metodo consultarReservas24horas: " + res);
     });
 }
 
 
 //====================================================================================================================================
-//
+// Consultar la cantidad de reservas para las últimas 24 horas
 //====================================================================================================================================
 function consultarNumeroReservas24horas() {
   $.ajax({
@@ -124,17 +146,25 @@ function consultarNumeroReservas24horas() {
     dataType: "json",
   })
     .done(function (res) {
-      if(res!= null){
-        var numeroRegistros24h=parseInt(res);
-        if(numeroRegistros24h > 0){
+      if (res != null) {
+        var numeroRegistros24h = parseInt(res);
+        if (numeroRegistros24h > 0) {
           totalReservas24horas(numeroRegistros24h);
-          $('#tituloReservas24Horas').text('Existen ' + numeroRegistros24h + ' reservas para las próximas 24 horas.')
+          $("#tituloReservas24Horas").text(
+            "Hay " + numeroRegistros24h + " reservas en las próximas 24 horas."
+          );
+        } else {
+          totalReservas24horas(0);
+          $("#tituloReservas24Horas").text(
+            "No hay reservas para las próximas 24 horas."
+          );
         }
+      } else {
+        totalReservas24horas(0);
+        $("#tituloReservas24Horas").text(
+          "No hay reservas para las próximas 24 horas."
+        );
       }
-      console.log(res);
-
-
-
     })
     .fail(function (res) {
       console.log("ERROR: en metodo consultarReservas24horas:" + res);
@@ -142,11 +172,8 @@ function consultarNumeroReservas24horas() {
 }
 
 
-
-
-
 //====================================================================================================================================
-//
+//Guardar una reserva
 //====================================================================================================================================
 function guardarReserva() {
   var nombre = document.getElementById("nombre").value;
@@ -179,7 +206,6 @@ function guardarReserva() {
     .done(function (res) {
       if (res == "OK") {
         mostrarAlerta('Nueva reserva creada con éxito.');
-        //alert("Los datos de la reserva se han modificado con éxito.");
         limpiarCamposRefrescar();
         $("#modalReserva").modal("toggle");
       } else {
@@ -192,30 +218,36 @@ function guardarReserva() {
 }
 
 //====================================================================================================================================
-//
+//Modificar una reservas concreta
 //====================================================================================================================================
 function modificarReserva(idReserva) {
-  $.ajax({
-    url: url,
-    data: enviarDatos("modificarReserva"),
-    type: "POST",
-    dataType: "json",
-  })
-    .done(function (res) {
-      if (res == "OK") {
-        //alert("Los datos de la reserva se han modificado con éxito.");
-        //limpiarCamposRefrescar();
-      } else {
-        alert(res);
-      }
+
+  if(validarFormulario()==true){
+      
+    $.ajax({
+      url: url,
+      data: enviarDatos("modificarReserva"),
+      type: "POST",
+      dataType: "json",
     })
-    .fail(function (res) {
-      console.log("ERROR: en metodo modificarReserva: " + res);
-    });
+      .done(function (res) {
+        if (res == "OK") {
+          mostrarAlerta("Reserva actualizada");
+          limpiarCamposRefrescar();
+          $('#modalReserva').modal("toggle");
+        } else {
+          alert(res);
+        }
+      })
+      .fail(function (res) {
+        console.log("ERROR: en metodo modificarReserva: " + res);
+      });
+  }
+
 }
 
 //====================================================================================================================================
-//
+//Eliminar un reserva
 //====================================================================================================================================
 function eliminarReserva(idReserva) {
   var idBorrar = $(this).data("id-reserva");
@@ -246,7 +278,7 @@ function eliminarReserva(idReserva) {
 }
 
 //====================================================================================================================================
-//
+//Función de apoyo a las anteriores para enviar los datos al servidor
 //====================================================================================================================================
 function enviarDatos(tarea) {
   return {
